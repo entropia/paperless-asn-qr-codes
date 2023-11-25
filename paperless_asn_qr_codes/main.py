a6b9eb50693b97ba0c1c8396de36c8a0d84b74f6
Marvin Gaube <dev@marvingaube.de>
2023-11-25 15:12:31 +0100

    feat: add support for choosing a label format

--- paperless_asn_qr_codes/main.py
+++ paperless_asn_qr_codes/main.py
@@ -20,10 +20,13 @@ def main():
                     description='CLI Tool for generating paperless ASN labels with QR codes')
     parser.add_argument('start_asn')
     parser.add_argument('output_file')
+    parser.add_argument('--format', choices=avery_labels.labelInfo.keys(), default="averyL4731")
     args = parser.parse_args()
     global startASN
     startASN = int(args.start_asn)
-    label = avery_labels.AveryLabel(4731)
+    label = avery_labels.AveryLabel(args.format)
     label.open(args.output_file)
-    label.render(render, 189 )
+    # by default, we render all labels possible on a single sheet
+    count = avery_labels.labelInfo[args.format][0]*avery_labels.labelInfo[args.format][1]
+    label.render(render, count )
     label.close()
