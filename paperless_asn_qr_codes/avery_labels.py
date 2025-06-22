613b00f2292787e9bf44754155821f92bab6a89d
Philipp Mahler <philipp.mahler@pmcgn.de>
2025-06-22 21:47:30 +0200

    added averyL7658

--- paperless_asn_qr_codes/avery_labels.py
+++ paperless_asn_qr_codes/avery_labels.py
@@ -48,6 +48,14 @@ labelInfo: dict[str, LabelInfo] = {
         margin=(11 * mm, 13.5 * mm),
         pagesize=A4,
     ),
+    "averyL7658": LabelInfo(
+        labels_horizontal=7,
+        labels_vertical=27,
+        label_size=(25.4 * mm, 10 * mm),
+        gutter_size=(2.5 * mm, 0.07 * mm),
+        margin=(9 * mm, 12.5 * mm),
+        pagesize=A4,
+    ),
     # 2.6 x 1 address labels
     "avery5160": LabelInfo(
         labels_horizontal=3,
